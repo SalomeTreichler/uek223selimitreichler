c930ce14123fab8f7fd420b1f83e412e5f8f28d8
Salome Treichler <salome.treichler@noseryoung.ch>
2020-04-03 10:40:16 +0200

    moved project folder

---
+++ uk/src/main/java/ch/noseryoung/uk/domainModels/article/Article.java
@@ -0,0 +1,53 @@
+package ch.noseryoung.uk.domainModels.article;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "value")
+    private double value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
